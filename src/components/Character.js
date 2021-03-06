1e2e1b64d0e32a055ea13b66bb2c910f8064b4f1
Nikolai Lvov <nikolailvov@Nikolais-MacBook-Pro.local>
2021-03-05 19:33:23 -0800

    I styled the character cards.

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,15 +1,51 @@
 // Write your Character component here
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+`
+
+const StyledSection = styled.section`
+    background-color: green;
+    color: orange;
+    padding: 1px;
+    width: 30%;
+    border-radius: 1rem;
+    margin: 2rem;
+    font-family: Arial, Helvetica, sans-serif;
+
+    h3{
+        color: blue;
+        text-transform: uppercase;
+        text-shadow: 2px 2px #ff0000;
+        &:hover {
+            color: yellow;
+            text-shadow: 4px 4px 3px green;
+        }
+        
+    }
+    
+`
+
 
 const Character = (props) => {
+    
     return (
-        <section className = 'card'>
-           <img src = {props.character.url} alt=''/>
-            <h3>{props.character.name}</h3>
-            <p>{props.character.height}cm</p>
-            <p>Films: {props.character.films}</p>
-
-        </section>
+        
+            <StyledDiv>
+                <StyledSection className = 'card'>
+                    <article>
+                    <h3>{props.character.name}</h3>
+                    <p>{props.character.height}cm</p>
+                    <p>{props.character.skin_color}</p> 
+                    </article>
+                </StyledSection>
+            </StyledDiv>
+        
 
     )
 }
